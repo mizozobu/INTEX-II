9c43427948e808b33a2af9bd44c694669ab39aea
= <riahsheffield@gmail.com>
2018-04-10 12:01:01 -0600

    update css for shopping cart page

--- catalog/templates/.cached_templates/base.htm.py
+++ catalog/templates/.cached_templates/base.htm.py
@@ -5,9 +5,9 @@ STOP_RENDERING = runtime.STOP_RENDERING
 __M_dict_builtin = dict
 __M_locals_builtin = locals
 _magic_number = 10
-_modified_time = 1523332679.2975585
+_modified_time = 1523380905.496534
 _enable_loop = True
-_template_filename = 'C:/users/scott laptop/documents/mariah/intex/intex-ii/catalog/templates/base.htm'
+_template_filename = 'C:/users/Scott Laptop/documents/Mariah/intex/intex-ii/catalog/templates/base.htm'
 _template_uri = 'base.htm'
 _source_encoding = 'utf-8'
 import django_mako_plus
@@ -19,26 +19,26 @@ def render_body(context,**pageargs):
     __M_caller = context.caller_stack._push_frame()
     try:
         __M_locals = __M_dict_builtin(pageargs=pageargs)
-        def header():
-            return render_header(context._locals(__M_locals))
-        def center():
-            return render_center(context._locals(__M_locals))
         def top():
             return render_top(context._locals(__M_locals))
-        def left():
-            return render_left(context._locals(__M_locals))
-        def bottom():
-            return render_bottom(context._locals(__M_locals))
         def right():
             return render_right(context._locals(__M_locals))
         def addonlink():
             return render_addonlink(context._locals(__M_locals))
-        self = context.get('self', UNDEFINED)
         def footer():
             return render_footer(context._locals(__M_locals))
-        STATIC_URL = context.get('STATIC_URL', UNDEFINED)
+        def bottom():
+            return render_bottom(context._locals(__M_locals))
         def maintenanceMessage():
             return render_maintenanceMessage(context._locals(__M_locals))
+        STATIC_URL = context.get('STATIC_URL', UNDEFINED)
+        def header():
+            return render_header(context._locals(__M_locals))
+        self = context.get('self', UNDEFINED)
+        def center():
+            return render_center(context._locals(__M_locals))
+        def left():
+            return render_left(context._locals(__M_locals))
         __M_writer = context.writer()
         __M_writer('<!DOCTYPE html>\r\n<html>\r\n    <meta charset="UTF-8">\r\n    <head>\r\n        <title>FOMO</title>\r\n\r\n')
         __M_writer('        <link rel="icon" href="')
@@ -209,6 +209,6 @@ def render_footer(context,**pageargs):
 
 """
 __M_BEGIN_METADATA
-{"filename": "C:/users/scott laptop/documents/mariah/intex/intex-ii/catalog/templates/base.htm", "uri": "base.htm", "source_encoding": "utf-8", "line_map": {"18": 0, "43": 2, "44": 9, "45": 9, "46": 9, "47": 12, "48": 12, "49": 12, "50": 13, "51": 13, "52": 14, "53": 14, "54": 15, "55": 15, "60": 16, "61": 18, "62": 21, "63": 22, "64": 22, "69": 31, "74": 37, "79": 44, "84": 51, "89": 54, "94": 58, "95": 63, "100": 65, "105": 81, "111": 16, "122": 31, "133": 37, "144": 44, "155": 51, "166": 54, "177": 58, "188": 65, "199": 81, "210": 199}}
+{"filename": "C:/users/Scott Laptop/documents/Mariah/intex/intex-ii/catalog/templates/base.htm", "uri": "base.htm", "source_encoding": "utf-8", "line_map": {"18": 0, "43": 2, "44": 9, "45": 9, "46": 9, "47": 12, "48": 12, "49": 12, "50": 13, "51": 13, "52": 14, "53": 14, "54": 15, "55": 15, "60": 16, "61": 18, "62": 21, "63": 22, "64": 22, "69": 31, "74": 37, "79": 44, "84": 51, "89": 54, "94": 58, "95": 63, "100": 65, "105": 81, "111": 16, "122": 31, "133": 37, "144": 44, "155": 51, "166": 54, "177": 58, "188": 65, "199": 81, "210": 199}}
 __M_END_METADATA
 """
--- catalog/templates/.cached_templates/cart.html.py
+++ catalog/templates/.cached_templates/cart.html.py
@@ -5,14 +5,14 @@ STOP_RENDERING = runtime.STOP_RENDERING
 __M_dict_builtin = dict
 __M_locals_builtin = locals
 _magic_number = 10
-_modified_time = 1523332679.250542
+_modified_time = 1523383127.4875216
 _enable_loop = True
-_template_filename = 'C:/users/scott laptop/documents/mariah/intex/intex-ii/catalog/templates/cart.html'
+_template_filename = 'C:/users/Scott Laptop/documents/Mariah/intex/intex-ii/catalog/templates/cart.html'
 _template_uri = 'cart.html'
 _source_encoding = 'utf-8'
 import django_mako_plus
 import django_mako_plus
-_exports = ['top', 'center', 'left', 'right']
+_exports = ['center', 'left', 'right']
 
 
 def _mako_get_namespace(context, name):
@@ -30,29 +30,22 @@ def render_body(context,**pageargs):
     __M_caller = context.caller_stack._push_frame()
     try:
         __M_locals = __M_dict_builtin(pageargs=pageargs)
-        tax = context.get('tax', UNDEFINED)
-        def center():
-            return render_center(context._locals(__M_locals))
-        def top():
-            return render_top(context._locals(__M_locals))
-        def left():
-            return render_left(context._locals(__M_locals))
-        items = context.get('items', UNDEFINED)
-        order = context.get('order', UNDEFINED)
         def right():
             return render_right(context._locals(__M_locals))
+        items = context.get('items', UNDEFINED)
+        tax = context.get('tax', UNDEFINED)
+        order = context.get('order', UNDEFINED)
+        def center():
+            return render_center(context._locals(__M_locals))
+        def left():
+            return render_left(context._locals(__M_locals))
         __M_writer = context.writer()
-        __M_writer('\r\n\r\n')
-        if 'parent' not in context._data or not hasattr(context._data['parent'], 'top'):
-            context['self'].top(**pageargs)
-        
-
         __M_writer('\r\n\r\n')
         if 'parent' not in context._data or not hasattr(context._data['parent'], 'center'):
             context['self'].center(**pageargs)
         
 
-        __M_writer('\r\n')
+        __M_writer('\r\n\r\n')
         if 'parent' not in context._data or not hasattr(context._data['parent'], 'left'):
             context['self'].left(**pageargs)
         
@@ -68,49 +61,41 @@ def render_body(context,**pageargs):
         context.caller_stack._pop_frame()
 
 
-def render_top(context,**pageargs):
-    __M_caller = context.caller_stack._push_frame()
-    try:
-        def top():
-            return render_top(context)
-        __M_writer = context.writer()
-        __M_writer('<h1>Cart</h1>')
-        return ''
-    finally:
-        context.caller_stack._pop_frame()
-
-
 def render_center(context,**pageargs):
     __M_caller = context.caller_stack._push_frame()
     try:
-        tax = context.get('tax', UNDEFINED)
-        items = context.get('items', UNDEFINED)
         order = context.get('order', UNDEFINED)
         def center():
             return render_center(context)
+        items = context.get('items', UNDEFINED)
+        tax = context.get('tax', UNDEFINED)
         __M_writer = context.writer()
-        __M_writer('\r\n<div class="content">\r\n  <table class="table">\r\n    <thead>\r\n      <tr>\r\n        <th>\r\n          Product\r\n        </th>\r\n        <th>\r\n          Quantity\r\n        </th>\r\n        <th>\r\n          Price\r\n        </th>\r\n        <th>\r\n          Item Total\r\n        </th>\r\n        <th>\r\n        </th>\r\n      </tr>\r\n    </thead>\r\n    <tbody>\r\n')
+        __M_writer('\r\n<div class="content top-buffer bottom-buffer">\r\n  <h1>Shopping Cart <a class="btn btn-primary" href="/catalog/checkout/')
+        __M_writer(str(order.id))
+        __M_writer('">Checkout Now</a></h1>\r\n\r\n    <table class="table">\r\n      <thead>\r\n        <tr>\r\n          <th class="spaceLeft">Image</th>\r\n          <th>\r\n            Product\r\n          </th>\r\n          <th>\r\n            Quantity\r\n          </th>\r\n          <th>\r\n            Price\r\n          </th>\r\n          <th>\r\n            Item Total\r\n          </th>\r\n          <th class="spaceRight">\r\n          </th>\r\n        </tr>\r\n      </thead>\r\n      <tbody>\r\n')
         for item in items:
-            __M_writer('        <tr>\r\n          <td>\r\n            ')
+            __M_writer('          <tr>\r\n            <td class="productTile spaceLeft">\r\n                <img src="')
+            __M_writer(str( item.product.image_url() ))
+            __M_writer('" alt="Image of ')
             __M_writer(str( item.product.name ))
-            __M_writer('\r\n          </td>\r\n          <td>\r\n            ')
+            __M_writer('" width="100px" />\r\n            </td>\r\n            <td>\r\n              ')
+            __M_writer(str( item.product.name ))
+            __M_writer('\r\n            </td>\r\n            <td>\r\n              ')
             __M_writer(str( item.quantity ))
-            __M_writer('\r\n          </td>\r\n          <td>\r\n            ')
+            __M_writer('\r\n            </td>\r\n            <td>\r\n              $')
             __M_writer(str( item.price))
-            __M_writer(' each\r\n          </td>\r\n          <td>\r\n            ')
+            __M_writer(' each\r\n            </td>\r\n            <td>\r\n              $')
             __M_writer(str( item.extended ))
-            __M_writer('\r\n          </td>\r\n          <td>\r\n            <a href="/catalog/delete/')
+            __M_writer('\r\n            </td>\r\n            <td class="spaceRight">\r\n              <a href="/catalog/delete/')
             __M_writer(str(item.id))
             __M_writer('/')
             __M_writer(str(order.id))
-            __M_writer('">Delete</a>\r\n          </td>\r\n        </tr>\r\n')
-        __M_writer('    </tbody>\r\n    <thead>\r\n      <tr>\r\n        <th>\r\n          Sales Tax:\r\n        </th>\r\n        <th>\r\n        </th>\r\n        <th>\r\n        </th>\r\n        <th>\r\n          ')
+            __M_writer('">Delete</a>\r\n            </td>\r\n          </tr>\r\n')
+        __M_writer('      </tbody>\r\n      <thead>\r\n        <tr>\r\n          <th class="spaceLeft">\r\n            Sales Tax:\r\n          </th>\r\n          <th>\r\n          </th>\r\n          <th>\r\n          </th>\r\n          <th>\r\n          </th>\r\n          <th>\r\n            $')
         __M_writer(str( tax.price ))
-        __M_writer('\r\n        </th>\r\n        <th>\r\n        </th>\r\n      </tr>\r\n      <tr>\r\n        <th>\r\n          Total:\r\n        </th>\r\n        <th>\r\n        </th>\r\n        <th>\r\n        </th>\r\n        <th>\r\n          ')
+        __M_writer('\r\n          </th>\r\n          <th>\r\n          </th>\r\n        </tr>\r\n        <tr>\r\n          <th>\r\n            Total:\r\n          </th>\r\n          <th>\r\n          </th>\r\n          <th>\r\n          </th>\r\n          <th>\r\n          </th>\r\n          <th>\r\n            $')
         __M_writer(str( order.total_price ))
-        __M_writer('\r\n        </th>\r\n        <th>\r\n        </th>\r\n      </tr>\r\n    </thead>\r\n  </table>\r\n  <a class="btn btn-default" href="/catalog/checkout/')
-        __M_writer(str(order.id))
-        __M_writer('">Checkout Now</a>\r\n</div>\r\n')
+        __M_writer('\r\n          </th>\r\n          <th>\r\n          </th>\r\n        </tr>\r\n      </thead>\r\n    </table>\r\n</div>\r\n')
         return ''
     finally:
         context.caller_stack._pop_frame()
@@ -122,6 +107,7 @@ def render_left(context,**pageargs):
         def left():
             return render_left(context)
         __M_writer = context.writer()
+        __M_writer('\r\n')
         return ''
     finally:
         context.caller_stack._pop_frame()
@@ -133,6 +119,7 @@ def render_right(context,**pageargs):
         def right():
             return render_right(context)
         __M_writer = context.writer()
+        __M_writer('\r\n')
         return ''
     finally:
         context.caller_stack._pop_frame()
@@ -140,6 +127,6 @@ def render_right(context,**pageargs):
 
 """
 __M_BEGIN_METADATA
-{"filename": "C:/users/scott laptop/documents/mariah/intex/intex-ii/catalog/templates/cart.html", "uri": "cart.html", "source_encoding": "utf-8", "line_map": {"29": 0, "45": 1, "50": 3, "55": 80, "60": 81, "65": 82, "71": 3, "77": 3, "83": 5, "92": 5, "93": 27, "94": 28, "95": 30, "96": 30, "97": 33, "98": 33, "99": 36, "100": 36, "101": 39, "102": 39, "103": 42, "104": 42, "105": 42, "106": 42, "107": 46, "108": 57, "109": 57, "110": 71, "111": 71, "112": 78, "113": 78, "119": 81, "130": 82, "141": 130}}
+{"filename": "C:/users/Scott Laptop/documents/Mariah/intex/intex-ii/catalog/templates/cart.html", "uri": "cart.html", "source_encoding": "utf-8", "line_map": {"29": 0, "43": 1, "48": 87, "53": 90, "58": 92, "64": 3, "73": 3, "74": 5, "75": 5, "76": 28, "77": 29, "78": 31, "79": 31, "80": 31, "81": 31, "82": 34, "83": 34, "84": 37, "85": 37, "86": 40, "87": 40, "88": 43, "89": 43, "90": 46, "91": 46, "92": 46, "93": 46, "94": 50, "95": 63, "96": 63, "97": 79, "98": 79, "104": 89, "110": 89, "116": 91, "122": 91, "128": 122}}
 __M_END_METADATA
 """
