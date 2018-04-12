a62eb5ae468f3b155aef1f646b44552195ec12ac
Hilary Nelson <hilarynelson38w@gmail.com>
2018-04-11 21:25:07 -0600

    fixed untracked files

---
+++ ad.py
@@ -0,0 +1,11 @@
+# from ldap3 import Server, Connection, ALL
+#
+# server = Server('128.187.61.58:80')
+# conn = Connection(server)
+# conn.bind()
+# import ldap
+# from django_auth_ldap.config import LDAPSearch
+#
+# AUTH_LDAP_BIND_DN = ""
+# AUTH_LDAP_BIND_PASSWORD = ""
+# AUTH_LDAP_USER_SEARCH = LDAPSearch("ou=users,dc=example,dc=com", ldap.SCOPE_SUBTREE, "(uid=%(user)s)")
